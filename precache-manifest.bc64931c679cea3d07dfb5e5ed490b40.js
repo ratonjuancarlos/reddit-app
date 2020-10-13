035833d0c8cc7bd3bc27311a9fe5b3409165349f
phiyano <phiyano@quarkslab.com>
2020-10-13 02:25:32 -0300

    Updates

---
+++ precache-manifest.bc64931c679cea3d07dfb5e5ed490b40.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a7854abc1da2b847053361db848c69d",
+    "url": "/Front-end/index.html"
+  },
+  {
+    "revision": "bde389a78ef49724f435",
+    "url": "/Front-end/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "baa8d2ce637a4a253362",
+    "url": "/Front-end/static/js/2.24e6e4da.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Front-end/static/js/2.24e6e4da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bde389a78ef49724f435",
+    "url": "/Front-end/static/js/main.97d7b811.chunk.js"
+  },
+  {
+    "revision": "e582b0f0b18f16eeb9eb",
+    "url": "/Front-end/static/js/runtime-main.0ccd86d9.js"
+  }
+]);
